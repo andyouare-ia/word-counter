304688eea3a274b9c1829c4d3fac7db6df2deed4
Nuria Toledano <ia.andyouare@gmail.com>
2022-03-26 08:13:43 +0000

    Added regex to remove non alphabetical characters

--- word_counter.rb
+++ word_counter.rb
@@ -1,2 +1,9 @@
-FILE_NAME = "romeo-juliet.txt"
-romeo_and_juliet = File.read(FILE_NAME)
+FILE_NAME = 'romeo-juliet.txt'
+
+def words_from_file(text_file)
+  File.read(text_file).downcase.gsub(/[^a-z]/, ' ').split
+end
+
+words = words_from_file(FILE_NAME)
+
+p words
