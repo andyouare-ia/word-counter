a4cb8b7c277f68366edba53c31ac4619f3acded6
Nuria Toledano <ia.andyouare@gmail.com>
2022-03-29 18:14:46 +0100

    added comments

--- word_counter.rb
+++ word_counter.rb
@@ -1,9 +1,19 @@
 FILE_NAME = 'romeo-juliet.txt'
 
+# Load only words from the file
 def words_from_file(text_file)
   File.read(text_file).downcase.gsub(/[^a-z]/, ' ').split
 end
 
+# Load the list of words in the text
 words = words_from_file(FILE_NAME)
 
-p words
+# Create a hash to store word counts
+word_count = {}
+
+# Iterate through the array to get the word count
+words.each do |word|
+  word_count[word] ? word_count[word] += 1 : word_count[word] = 1
+end
+
+p word_count['romeo']
